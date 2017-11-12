29c44d1f8b97758babf8a345ccb669ad12d46cc7
SharktallicA <22660310+SharktallicA@users.noreply.github.com>
2017-11-12 20:17:44 +0000

    Add Entity and derived classes

---
+++ Entity.cpp
@@ -0,0 +1,18 @@
+#include "Entity.h"
+
+int Entity::getX()
+{
+	return intX;
+}
+int Entity::getY()
+{
+	return intY;
+}
+void Entity::setX(int intNewX)
+{
+	intX = intNewX;
+}
+void Entity::setY(int intNewY)
+{
+	intY = intNewY;
+}
---
+++ Entity.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "Utility.h"
+#include <string>
+
+using namespace std;
+
+class Entity
+{
+private:
+	int intX;
+	int intY;
+public:
+	Entity() {};
+
+	int getX();
+	int getY();
+	void setX(int);
+	void setY(int);
+};
+
+class Human : public Entity
+{
+private:
+	string strName;
+private:
+	Human(string strNewName) { strName = strNewName; };
+
+	string getName();
+};
+
+class Zombie : public Entity
+{
+
+};
